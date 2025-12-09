c81449bcc2455377b5b9d42ce6267f97ea98f440
Wonseok Cho <jws020501@naver.com>
2025-12-09 09:10:52 +0900

    hotfix:빌드에러 수정

--- yobuddy/src/main.js
+++ yobuddy/src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VueApexCharts from 'vue3-apexcharts'
+import { useAuthStore } from '@/store/authStore'
 
 const app = createApp(App)
 app.use(createPinia())
