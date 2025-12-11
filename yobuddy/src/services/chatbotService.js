cdfcc748e42838f56883b07fe237f7dfb826c91b
Jintory <poketmonlsj@naver.com>
2025-12-11 18:54:09 +0900

    fix: chatbotService URL

--- yobuddy/src/services/chatbotService.js
+++ yobuddy/src/services/chatbotService.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const CHATBOT_API = "http://13.209.18.145:8000"
+
 export function askChatbot(question) {
-  return axios.post('/api/faq/ask', { question })
+  return axios.post(`${CHATBOT_API}/api/faq/ask`, { question })
 }
